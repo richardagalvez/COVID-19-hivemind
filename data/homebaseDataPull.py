6ab49da41efb1e1b0918cb007f163995ef95ed31
Richard Galvez <richard.galvez@anheuser-busch.com>
2020-05-13 13:02:03 -0400

    updating may 13

--- data/homebaseDataPull.py
+++ data/homebaseDataPull.py
@@ -7,14 +7,15 @@ workers.index = workers.index + 1
 workers.loc[0] = workers.columns
 workers.sort_index(inplace=True)
 
-may_dates   = [f'5-{day}-2020' for day in range(10, 0, -1)]
+may_dates   = [f'5-{day}-2020' for day in range(11, 0, -1)]
 april_dates = [f'4-{day}-2020' for day in range(30, 0, -1)]
 march_dates = [f'3-{day}-2020' for day in range(31, 0, -1)]
 
 dates = may_dates + april_dates + march_dates
 workers.columns = ['msa'] + dates
 workers.set_index('msa', inplace=True)
-for column in workers.columns: workers[column] = pd.to_numeric(workers[column].str.replace('%', ''))
+for column in workers.columns: 
+    workers[column] = pd.to_numeric(workers[column].str.replace('%', ''))
 workers = workers.T
 workers.index = pd.to_datetime(workers.index)
 workers.sort_index(inplace=True)
